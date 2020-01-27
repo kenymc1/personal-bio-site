6d639078cdc325efb31bcabd9baee125b7273dec
Kenny Mc <kenymc1@gmail.com>
2020-01-26 22:01:29 -0600

    added site

---
+++ src/javascripts/main.js
@@ -0,0 +1,13 @@
+import 'bootstrap';
+// import axios from 'axios';
+
+// const examplePromise = () => new Promise((resolve, reject) => {
+//   axios.get('http://localhost:3001/example')
+//     .then((data) => {
+//       resolve(data);
+//     })
+//     .catch((error) => {
+//       reject(error);
+//     });
+// });
+
